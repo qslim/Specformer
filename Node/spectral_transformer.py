ccfa325118f7f6a2f01b586f2580ba69bd57f0db
“qslim” <mqyang1s@163.com>
2024-04-29 16:41:57 +0800

    update

--- Node/spectral_transformer.py
+++ Node/spectral_transformer.py
@@ -53,6 +53,90 @@ class Transformer(nn.Module):
         return x
 
 
+# class Specformer(nn.Module):
+#
+#     def __init__(self, nclass, nfeat, nlayer=1, hidden_dim=128, nheads=1,
+#                  tran_dropout=0.0, feat_dropout=0.0, prop_dropout=0.0, norm='none'):
+#         super(Specformer, self).__init__()
+#
+#         self.nfeat = nfeat
+#         self.nlayer = nlayer
+#         self.nheads = nheads
+#         self.hidden_dim = hidden_dim
+#
+#         self.feat_encoder = nn.Sequential(
+#             nn.Dropout(feat_dropout),
+#             nn.Linear(nfeat, hidden_dim),
+#             nn.ReLU(),
+#             nn.Linear(hidden_dim, nclass),
+#             nn.Dropout(feat_dropout),
+#         )
+#
+#         # for arxiv & penn
+#         self.linear_encoder = nn.Sequential(
+#             nn.Dropout(feat_dropout),
+#             nn.Linear(nfeat, hidden_dim),
+#         )
+#         self.classify = nn.Linear(hidden_dim, nclass)
+#
+#         self.eig_encoder = SineEncoding(hidden_dim)
+#         self.decoder = nn.Linear(hidden_dim, 1)
+#
+#         self.transformer = Transformer(hidden_dim, nheads, tran_dropout)
+#
+#
+#         if norm == 'none':
+#             layer_dim = nclass
+#             self.norm = None
+#         else:
+#             layer_dim = hidden_dim
+#             if norm == 'layer':  # Arxiv
+#                 self.norm = nn.LayerNorm(hidden_dim)
+#             elif norm == 'batch':  # Penn
+#                 self.norm = nn.BatchNorm1d(hidden_dim)
+#             else:  # Others
+#                 self.norm = None
+#         self.layers = nn.ModuleList([Transformer(layer_dim, nheads, prop_dropout) for i in range(nlayer)])
+#
+#     def forward(self, e, u, x):
+#         N = e.size(0)
+#         ut = u.permute(1, 0)
+#
+#         if self.norm is None:
+#             h = self.feat_encoder(x)
+#         else:
+#             h = self.linear_encoder(x)
+#
+#         eig = self.eig_encoder(e)  # [N, d]
+#
+#         eig = self.transformer(eig)
+#
+#         new_e = self.decoder(eig)  # [N, m]
+#
+#         x = h
+#
+#         for conv in self.layers:
+#             utx = ut @ h
+#             h = conv(new_e * utx)
+#
+#             h = u @ h
+#
+#             if self.norm is None:
+#                 h = F.gelu(h)
+#             else:
+#                 h = self.norm(h)
+#                 h = F.relu(h)
+#
+#         h = h + x
+#
+#         if self.norm is None:
+#             return h
+#         else:
+#             h = self.feat_dp2(h)
+#             h = self.classify(h)
+#             return h
+
+
 class Specformer(nn.Module):
 
     def __init__(self, nclass, nfeat, nlayer=1, hidden_dim=128, nheads=1,
@@ -72,67 +156,30 @@ class Specformer(nn.Module):
             nn.Dropout(feat_dropout),
         )
 
-        # for arxiv & penn
-        self.linear_encoder = nn.Sequential(
-            nn.Dropout(feat_dropout),
-            nn.Linear(nfeat, hidden_dim),
-        )
-        self.classify = nn.Linear(hidden_dim, nclass)
-
         self.eig_encoder = SineEncoding(hidden_dim)
         self.decoder = nn.Linear(hidden_dim, 1)
 
         self.transformer = Transformer(hidden_dim, nheads, tran_dropout)
 
-
-        if norm == 'none':
-            layer_dim = nclass
-            self.norm = None
-        else:
-            layer_dim = hidden_dim
-            if norm == 'layer':  # Arxiv
-                self.norm = nn.LayerNorm(hidden_dim)
-            elif norm == 'batch':  # Penn
-                self.norm = nn.BatchNorm1d(hidden_dim)
-            else:  # Others
-                self.norm = None
+        layer_dim = nclass
+        self.norm = None
         self.layers = nn.ModuleList([Transformer(layer_dim, nheads, prop_dropout) for i in range(nlayer)])
-    
+
     def forward(self, e, u, x):
         N = e.size(0)
         ut = u.permute(1, 0)
-
-        if self.norm is None:
-            h = self.feat_encoder(x)
-        else:
-            h = self.linear_encoder(x)
-
+        h = self.feat_encoder(x)
         eig = self.eig_encoder(e)  # [N, d]
-
         eig = self.transformer(eig)
-
         new_e = self.decoder(eig)  # [N, m]
-
         x = h
 
         for conv in self.layers:
             utx = ut @ h
             h = conv(new_e * utx)
-
             h = u @ h
-
-            if self.norm is None:
-                h = F.gelu(h)
-            else:
-                h = self.norm(h)
-                h = F.relu(h)
+            h = F.gelu(h)
 
         h = h + x
 
-        if self.norm is None:
-            return h
-        else:
-            h = self.feat_dp2(h)
-            h = self.classify(h)
-            return h
-
+        return h
